850f5db8e69534317634b04fd65bdb566bbe2fb7
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-08-01 16:35:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [city, setCity] = useState("São Paulo")
+
+  const [weatherForecast, setWeatherForecast] = useState(null)
+
+  const handleChange = (e) => {
+    setCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    fetch(`
+    http://api.weatherapi.com/v1/current.json?key=8f31718a6fe04ae792841222220507&q=${city}&lang=pt`
+    )
+      .then((response) => {
+        if (response.status == 200) {
+          return response.json()
+        }
+      })
+      .then((data) => {
+        setWeatherForecast(data)
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expanded-md navbar-dark bg-dark mb-4">
+        <a className="navbar-brand text-white" href="#toque">
+          Previsão do Tempo
         </a>
-      </header>
+      </nav>
+
+      <main className="container">
+        <div className="jumbotron">
+          <h1>
+            Verifique a previsão do tempo da sua cidade
+          </h1>
+          <p className="lead">
+            Digite o nome da sua cidade e clique em pesquisar
+          </p>
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <input
+                onChange={handleChange}
+                className="form-control"
+                value={city} />
+            </div>
+          </div>
+          <button
+            onClick={handleSearch}
+            className="btn btn-primary btn-lg"
+          >
+            Pesquisar
+          </button>
+
+          {
+            weatherForecast ? (
+              <div>
+                <div className='mt-4 d-flex align-items-center p-4'>
+                  <div>
+                    <img src={weatherForecast.current.condition.icon} />
+                  </div>
+                  
+                <div>
+
+                  <h3> Clima:{weatherForecast.current.condition.text}</h3>
+                  <p>
+                    Temperatura:{weatherForecast.current.temp_c}°c
+                  </p>
+                </div>
+
+                </div>
+              </div>
+            ) : null
+          }
+
+        </div>
+      </main>
     </div>
   );
 }
